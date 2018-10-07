6910c07b411a7e5c76845913aaa3bd8dc87ceb74
nayantharar <42238330+nayantharar@users.noreply.github.com>
2018-10-07 14:46:25 +0000

    Update lab7_q9.cpp

--- lab7_q9.cpp
+++ lab7_q9.cpp
@@ -1,28 +1,33 @@
+
 //include the library
 #include<iostream>
 using namespace std;
+
 //declaration and definition of the recursive function 
- int findfac(int i,int j,int s)
-{ 	//loop terminator
-	if(j>i){
+ int findfac(int a,int n,int s)
+{ 	
+	 //loop terminator
+	if(n>a){
 		cout<<" factorial of the given number = "<<s<<endl;
 	}
  	else{   //multiplying each element to the previous product
-		s*=j;
+		s*=n;
 		//increment
-		j++;
+		n++;
 		//recursive call
-		findfac(i,j,s);	
+		findfac(a,n,s);	
 		return 0;
 	}
 	return 0;
 }
-//main function
-int main(){
-	//input from user
+//write the main function
+int main()
+    {
+	//enter any number
 	int a;
 	cout<<"\n Enter any number ";
 	cin>>a;
 	//calling recursive function
 	findfac(a,1,1);
 	return 0;
+    }
